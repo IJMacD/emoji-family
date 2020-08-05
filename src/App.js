c6d0092a5d7946da08b4b9df3ad0cecc0514b0f1
Iain MacDonald <IJMacD@gmail.com>
2020-08-05 13:06:12 +0800

    Add skin tone support

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import './App.css';
 
+const SKIN_MODIFIERS = "\u{1f3fb}\u{1f3fc}\u{1f3fd}\u{1f3fe}\u{1f3ff}";
+const HAIR_MODIFIERS = "\u{1f9b0}\u{1f9b1}\u{1f9b2}\u{1f9b3}";
+
 function App() {
   const [ adults, setAdults ] = React.useState(["👩", "👨"]);
   const [ children, setChildren ] = React.useState([]);
+
+  const [ skin, setSkin ] = React.useState("");
+  const [ hair, setHair ] = React.useState("");
 
   return (
     <div className="App">
@@ -26,11 +32,23 @@ function App() {
         <section className="section-add">
           <h1>Add Members</h1>
           <p>
-            <button onClick={() => setAdults([...adults, "👨"])}>👨</button>
-            <button onClick={() => setAdults([...adults, "👩"])}>👩</button>
-            <button onClick={() => setChildren([...children, "👦"])}>👦</button>
-            <button onClick={() => setChildren([...children, "👧"])}>👧</button>
+            <button onClick={() => setAdults([...adults, "👨" + skin + hair])}>{`👨${skin}${hair}`}</button>
+            <button onClick={() => setAdults([...adults, "👩" + skin + hair])}>{`👩${skin}${hair}`}</button>
+            <button onClick={() => setChildren([...children, "👦" + skin + hair])}>{`👦${skin}${hair}`}</button>
+            <button onClick={() => setChildren([...children, "👧" + skin + hair])}>{`👧${skin}${hair}`}</button>
           </p>
+          <p style={{display:"flex",justifyContent:"center"}}>
+            <button onClick={() => setSkin("")}>Default</button>
+            {
+              [...SKIN_MODIFIERS].map(c => <button onClick={() => setSkin(c)}>{c}</button>)
+            }
+          </p>
+          {/* <p style={{display:"flex",justifyContent:"center"}}>
+            <button onClick={() => setHair("")}>Default</button>
+            {
+              [...HAIR_MODIFIERS].map(c => <button onClick={() => setHair("\u200d" + c)}>{c}</button>)
+            }
+          </p> */}
         </section>
       </div>
     </div>
