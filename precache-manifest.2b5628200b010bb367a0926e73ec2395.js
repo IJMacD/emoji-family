43be54149e4b3a41ccd248a2752415e87d4e2c9e
Iain MacDonald <IJMacD@gmail.com>
2020-08-05 12:20:24 +0800

    Updates

---
+++ precache-manifest.2b5628200b010bb367a0926e73ec2395.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd14d339d28b3d414816c1c13a230309",
+    "url": "/emoji-family/index.html"
+  },
+  {
+    "revision": "086ad1edf2196f1e9f11",
+    "url": "/emoji-family/static/css/main.888d9f19.chunk.css"
+  },
+  {
+    "revision": "9eafd1cd163f3edd847e",
+    "url": "/emoji-family/static/js/2.efb03ba1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/emoji-family/static/js/2.efb03ba1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "086ad1edf2196f1e9f11",
+    "url": "/emoji-family/static/js/main.155fce36.chunk.js"
+  },
+  {
+    "revision": "f82a8e13f1a4832ded65",
+    "url": "/emoji-family/static/js/runtime-main.008301bb.js"
+  }
+]);
